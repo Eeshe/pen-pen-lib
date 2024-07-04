4bfe6d91d40052b7ac1de2cac1288dcba56a4758
Eeshe <eeshedev@proton.me>
2024-07-03 21:01:14 -0400

    fix: Fixed folia server detection

--- src/main/java/me/eeshe/penpenlib/util/FoliaUtil.java
+++ src/main/java/me/eeshe/penpenlib/util/FoliaUtil.java
@@ -1,11 +1,13 @@
 package me.eeshe.penpenlib.util;
 
-import org.bukkit.Bukkit;
-
 public class FoliaUtil {
-    private static final boolean IS_FOLIA = Bukkit.getVersion().contains("Folia");
 
     public static boolean isFolia() {
-        return IS_FOLIA;
+        try {
+            Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
     }
 }
